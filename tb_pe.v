543d67f945c94cea471609bc233260045ab105ed
chihyeong-an <60175460+chihyeong-an@users.noreply.github.com>
2023-01-09 15:16:09 +0900

    Update tb_pe.v

--- tb_pe.v
+++ tb_pe.v
@@ -1,8 +1,8 @@
 module tb_pe();
 
     reg clk, active, wwrite;
-    reg [7:0] datain, win;
-    reg [15:0] sumin;
+    reg [7:0] datain, w_in;
+    reg [15:0] sum_in;
 
     wire [15:0] maccout;
     wire [7:0] dataout, wout;
@@ -14,8 +14,8 @@ module tb_pe();
         .clk(clk),
         .active(active),
         .datain(datain),
-        .win(win),
-        .sumin(sumin),
+        .win(w_in),
+        .sumin(sum_in),
         .wwrite(wwrite),
         .maccout(maccout),
         .dataout(dataout),
@@ -26,31 +26,31 @@ module tb_pe();
 
     always begin
         #5;
-        clk = ~clk;
+        clk = ~clk; // clock diversion per 5 time scale 
     end // always
 
     initial begin
-        clk = 1'b0;
-        active = 1'b0;
+        clk = 1'b0; 
+        active = 1'b0; 
         wwrite = 1'b0;
         datain = 8'h00;
-        win = 8'h00;
-        sumin = 16'h0000;
+        w_in = 8'h00;
+        sum_in = 16'h0000;
 
         #100;
 
-        wwrite = 1'b1;
+        wwrite = 1'b1; 
 
         for (i = 0; i < 64; i = i + 1) begin
             #10;
-            win = win + 8'h04;
+            w_in = w_in + 8'h04; // w_in is changed but sustain mac_out = 0, because of data_in = 0, sum_in= 0 >> mult_result = data_in* w_in =  0
         end
 
         wwrite = 1'b0;
 
         for (i = 0; i < 64; i = i + 1) begin
             #10;
-            win = win + 8'h02;
+            w_in = w_in + 8'h02; // w_out = 8'hAA because of wwrite =0 
         end
 
     end // initial
